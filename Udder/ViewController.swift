ee0283aff3f833a2a8882e65611df131c9eba20d
Tóth Zoltán <tothzoltan01181996@gmail.com>
2020-04-15 20:15:55 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Tóth Zoltán on 2020. 04. 15..
+//  Copyright © 2020. Tóth Zoltán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
